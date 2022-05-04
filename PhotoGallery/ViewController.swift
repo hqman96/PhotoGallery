9780edd28a76b761d0fdc16d0ad6e0c8aabc3772
Georgiy Kiselev <hqman96@gmail.com>
2022-05-04 03:37:17 +0300

    Initial Commit

---
+++ PhotoGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Георгий on 04.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
