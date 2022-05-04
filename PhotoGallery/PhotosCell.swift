e380f047e6060412eaa23f9a2cef61d272cbbfbd
Georgiy Kiselev <hqman96@gmail.com>
2022-05-04 19:51:05 +0300

    Add PhotosCell

---
+++ PhotoGallery/PhotosCell.swift
@@ -0,0 +1,44 @@
+//
+//  PhotosCell.swift
+//  PhotoGallery
+//
+//  Created by Георгий on 04.05.2022.
+//
+
+import UIKit
+
+class PhotosCell: UICollectionViewCell {
+    
+    static let reuseId = "photoCell"
+    
+    private let photoImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        photoImageView.image = nil
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupPhotoImageView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupPhotoImageView() {
+        addSubview(photoImageView)
+        photoImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        photoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        photoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+
+    }
+}
