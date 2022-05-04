ea0a3341d312d1be1cf089cb01f0cfd83e2a64b7
Georgiy Kiselev <hqman96@gmail.com>
2022-05-04 05:55:28 +0300

    Add setupCollectionView()         setupNavigationBar()

--- PhotoGallery/PhotosCollectionViewController.swift
+++ PhotoGallery/PhotosCollectionViewController.swift
@@ -13,5 +13,30 @@ class PhotosCollectionViewController: UICollectionViewController {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .orange
+        setupCollectionView()
+        setupNavigationBar()
+    }
+    
+    private func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
+        
+    }
+    
+    private func setupNavigationBar() {
+        let titleLabel = UILabel()
+        titleLabel.text = "GALLERY"
+        titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        titleLabel.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
     }
 }
